666635527e1d2600225fc124fb87923a8a79c418
Velmurugan R <cenavel2@gmail.com>
2020-01-19 08:23:28 +0530

    added notificationChannel

--- localnotification/src/LocalNotificationReceiver.java
+++ localnotification/src/LocalNotificationReceiver.java
@@ -1,13 +1,14 @@
 package org.godotengine.godot;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import androidx.core.app.NotificationCompat;
 import android.util.Log;
 import android.net.Uri;
@@ -17,7 +18,8 @@ import android.media.RingtoneManager;
 public class LocalNotificationReceiver extends BroadcastReceiver {
     private static final String TAG = "Notification";
     private static final String DEFAULT_CHANNEL_ID = "10001";
-	
+    private static final String DEFAULT_CHANNEL_NAME = "Notify";
+
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		int notificationId = intent.getIntExtra("notification_id", 0);
@@ -45,7 +47,20 @@ public class LocalNotificationReceiver extends BroadcastReceiver {
 
 		NotificationManager manager = (NotificationManager) context
 				.getSystemService(Context.NOTIFICATION_SERVICE);
-
+		
+        // notification channel
+		NotificationChannel nc = null;
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+			nc = new NotificationChannel(DEFAULT_CHANNEL_ID, DEFAULT_CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+			nc.enableLights(true);
+			nc.enableVibration(true);
+			nc.setLightColor(Color.GRAY);
+			nc.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+		}
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			manager.createNotificationChannel(nc);
+	}
 		manager.notify(notificationId, notification);
 	}
 }
